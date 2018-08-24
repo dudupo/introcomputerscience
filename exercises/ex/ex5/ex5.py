47002ced8696b57bccd7f980873308592dda4cc9
dudu <dudupo51@gmail.com>
2018-08-24 17:24:17 +0300

    keep working on exercise 5

--- exercises/ex/ex5/ex5.py
+++ exercises/ex/ex5/ex5.py
@@ -8,20 +8,51 @@
                         -> True , None, $
 '''
 
+def crossword( matrix , wards , directionsstring , output_file):
 
+    thereisoppsite = False
+    opposite =  {'r' : 'l' , 'u' : 'd' ,'x' : 'y' , 'z' : 'w' }
+    for direction in directionsstring:
+        if direction in opposite :
+            directionsstring += opposite[direction]
+            thereisoppsite = True
 
-def crossword( matrix ):
+    def reverseward(ward):
+        if len(ward) > 1 :
+            return ward[-1] + reverseward(ward[:-1])
+        else :
+            return ward[-1]
+
+    if thereisoppsite :
+        wards += [ reverseward(ward) for ward in wards ]
+
+    directions = {
+        'd' : ( lambda i , j : i , j - 1 ) ,
+        'l' : ( lambda i , j : i + 1 , j ) ,
+        'y' : ( lambda i , j : i - 1 , j - 1 ) ,
+        'w' : ( lambda i , j : i + 1 , j + 1 )
+    }
+    directions = { key : value for key , value in directions.items() \
+     if key in directionsstring }
+    wards = [ ward + '$' for ward in wards ]
+    defaultstate = { ward[0] : (ward[1] , direction) \
+     for direction , f in directions.items() for ward in wards }
+
     automat =  { }
-
-    for i , line in enumerate( matrix ) :
-        for j , X in enumerate( line )  :
-            positive, directions, _next  = automat[(i , j)](X)
-            if positive :
-                if _next == '$':
-                    pass # updade
-                else:
-                     
-    pass
+    for j , X in enumerate( line )  :
+        for i , line in enumerate( matrix ) :
+            state = automat[(i , j)] if (i , j) in automat else defaultstate
+            if X in state :
+                for direction , _next  in state[X] :
+                    if _next == '$':
+                        pass # updade
+                    else:
+                        _i , _j = directions[direction]( i , j )
+                        automat[ (_i , _j) ] = { } \
+                         if (_i , _j) not in automat else automat[(_i , _j)]
+                        automat[ (_i , _j) ][ _next ] = [ ] \
+                         if _next not in automat[(_i , _j)] else automat[(_i , _j)][_next]
+                        automat[ (_i , _j)][ _next ].append( direction , next( _next ) )
 
 import os
 
