d5994497eff168e42e0fc0003fcc27f2555219cc
dudu <dudupo51@gmail.com>
2018-08-26 08:28:13 +0300

    works

--- exercises/ex/ex5/ex5.py
+++ exercises/ex/ex5/ex5.py
@@ -7,15 +7,17 @@
                         -> False, None, None
                         -> True , None, $
 '''
-from copy import copy
+from copy import deepcopy , copy
 
 def crossword( matrix , wards , directionsstring , output_file):
 
+    print(matrix)
     thereisoppsite = False
     opposite =  {'r' : 'l' , 'u' : 'd' ,'x' : 'y' , 'z' : 'w' }
+    rdirectionsstring = ""
     for direction in directionsstring:
         if direction in opposite :
-            directionsstring += opposite[direction]
+            rdirectionsstring += opposite[direction]
             thereisoppsite = True
 
     def reverseward(ward):
@@ -24,34 +26,53 @@ def crossword( matrix , wards , directionsstring , output_file):
         else :
             return ward[-1]
 
-    out = { ward : 0 for ward in wards }
+    out = { ward : int(0) for ward in wards }
     wards = [ (ward , ward ) for ward in wards ]
+    rwards = []
     if thereisoppsite :
-        wards += [ (reverseward(ward) , ward) for ward , x  in wards ]
+        rwards = [ (reverseward(ward) , ward) for ward , x  in wards ]
 
-    wards = [ list(_str) + ['$' ] +  [ward] for _str , ward in wards ]
+    wards  = [ list(_str) + ['$' ] +  [ward] for _str , ward in wards ]
+    rwards = [ list(_str) + ['$']  +  [ward] for _str , ward in rwards]
 
     print(len(wards))
 
     directions = {
         'd' : ( lambda i , j : (i , j + 1 )) ,
         'l' : ( lambda i , j : (i + 1 , j )) ,
-        'y' : ( lambda i , j : (i - 1 , j + 1 )) ,
-        'w' : ( lambda i , j : (i + 1 , j - 1 ))
+        'w' : ( lambda i , j : (i - 1 , j + 1 )) ,
+        'y' : ( lambda i , j : (i + 1 , j + 1 ))
     }
-    directions = { key : value for key , value in directions.items() \
+    print(wards[1][1:])
+    rdirections = { key : value for key , value in directions.items() \
+     if key in rdirectionsstring }
+
+    adirections = { key : value for key , value in directions.items() \
      if key in directionsstring }
 
-    defaultstate = { ward[0] :[ (iter(ward[1:]) , direction) \
-     for direction , f in directions.items()] for ward in wards }
+    defaultstate = { }
+    for _wards , _directions in zip([wards , rwards ] , [ adirections , rdirections ]) :
+        for ward in _wards:
+            if ward[0] not in defaultstate:
+                defaultstate[ward[0]] = []
+            for direction , f in _directions.items():
+                defaultstate[ward[0]].append (  (iter(ward[1:]) , direction))
 
-    print(defaultstate['d'])
-
+    '''
+    defaultstate =  { **{ ward[0] :[ (iter(ward[1:]) , direction) \
+     for direction , f in adirections.items()] for ward in wards } , \
+     ** { ward[0] :[ (iter(ward[1:]) , direction) \
+      for direction , f in rdirections.items()] for ward in rwards } }
+    '''
     #print(defaultstate)
     automat =  { }
+    for j , line in enumerate( matrix ):
+        for i , X in enumerate( line ) :
+            automat[(i , j)] = deepcopy(defaultstate)
+
     for j , line in enumerate( matrix )  :
         for i , X in enumerate( line ) :
-            state = automat[(i , j)] if (i , j) in automat else defaultstate
+            state = automat[(i , j)]
             if X in state :
                 for _nextgen , direction in state[X] :
                     _nextgen = copy(_nextgen)
@@ -61,11 +82,11 @@ def crossword( matrix , wards , directionsstring , output_file):
 
                     else:
                         _i , _j = directions[direction]( i , j )
-                        automat[ (_i , _j) ] = defaultstate \
-                         if (_i , _j) not in automat else automat[(_i , _j)]
-                        automat[ (_i , _j) ][ _next ] = [ ] \
-                         if _next not in automat[(_i , _j)] else automat[(_i , _j)][_next]
-                        automat[ (_i , _j)][ _next ].append( (copy( _nextgen ) , direction)  )
+                        #print( "_i : {0} ~ _j : {1}".format(_i , _j))
+                        if (_i , _j) in automat :
+                            automat[ (_i , _j) ][ _next ] = [ ] \
+                             if _next not in automat[(_i , _j)] else automat[(_i , _j)][_next]
+                            automat[ (_i , _j)][ _next ].append( (copy( _nextgen ) , direction)  )
 
     with open(output_file, 'w') as _file :
         for ward , _count in out.items() :
--- exercises/ex/ex5/run.sh
+++ exercises/ex/ex5/run.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+python ./ex5.py ./part_a/word_list.txt ./part_a/mat.txt "y" ./out1.txt
+python ./ex5.py ./part_a/word_list.txt ./part_a/mat.txt "u" ./out2.txt
+python ./ex5.py ./part_a/word_list.txt ./part_a/mat.txt "r" ./out3.txt
+python ./ex5.py ./part_a/word_list.txt ./part_a/mat.txt "ud" ./out4.txt
 python ./ex5.py ./part_a/word_list.txt ./part_a/mat.txt "udlrwxyz" ./out.txt
