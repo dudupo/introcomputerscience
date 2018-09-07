476c0bbf066cc43d7035db74ffedbf3495768a03
dudu <dudupo51@gmail.com>
2018-09-07 17:41:55 +0300

    lifting the code standart at execerice 11

--- exercises/ex/ex11/ex11_improve_backtrack.py
+++ exercises/ex/ex11/ex11_improve_backtrack.py
@@ -1,157 +1,150 @@
 from ex11_map_coloring import read_adj_file, COLORS, check_map
-
-#define dkeysratelcv(X)
-dkeysrateg(X, 0)
-#enddef
-#define dkeysrate(X)
-dkeysrateg(X, True)
-#enddef
-#define dkeysrateg(X,retv)
-def decrease_keys(country):
-    ret = retv
-    dforadj(in)
-                X(remove)
-    return ret
-def increase_keys(country):
-    dforadj(not in)
-                dcolor(append)
-#enddef
-#define dpop()
-if len(heap) == 0 :
-    return True
-negdegree , country = heapq.heappop(heap)
-#enddef
-#define dforward(back_track)
-if check_map(state, country, adj_dict) and \
- back_track(heap, state, adj_dict, colors):
-    return True
-#enddef
-#define dsetcolordir()
-dsetcolor(colors[country])
-#enddef
-#define dsetcolor(colors)
-for color in colors :
-    state[country] = color
-#enddef
-#define dforadj(conn)
-for adj_country in adj_dict[country]:
-    if state[adj_country] is None:
-        if state[country] conn colors[adj_country]:
-#enddef
-#define dcoloroptionscount(X)
-dcolor(X)
-ret += len(colors[adj_country])
-#enddef
-#define dcolor(X)
-colors[adj_country].X( state[country]  )
-heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-#enddef
-#define ddcolor(X)
-colors[adj_country].X( state[country]  )
-if len(colors[adj_country]) == 0 :
-    return False
-heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-#enddef
+from copy import deepcopy
 import heapq
 
-from copy import deepcopy , copy
+class BackTrack:
+    def __init__ (self, adj_dict, colors):
+        self.heap = adj_dict.keys()
+        self.colors = { **{ 0  : deepcopy(colors)   } ,
+        **{key : deepcopy(colors) for key in adj_dict.keys() }}
+        self.state = { key : None for key in adj_dict.keys() }
+        self.adj_dict = adj_dict
+    #abs
+    def pop(self):
+        return self.heap.pop()
 
-def initilaize(adj_dict, colors):
-    heap = [( 0 , key ) for key , value in adj_dict.items() ]
-    heapq.heapify(heap)
-    state = { key : None for key in adj_dict.keys() }
-    colors = { **{ 0  : deepcopy(colors)   } ,
-    **{key : deepcopy(colors) for key in adj_dict.keys() }}
-    return heap, state, colors
+    def settingAll(self):
+        return len(self.heap) == 0
 
-def back_track(heap , state, adj_dict, colors, hooking=False):
-    dpop()
-    dsetcolor(colors)
-        dforward(back_track)
-    state[country] = None
-    heapq.heappush(heap , (negdegree , country) )
-    return False
+    def opthions(self , country):
+        return self.colors[country]
 
+    def setColor(self, country, color):
+        self.state[country] = color
+    def undoColor(self, country, color):
+        self.state[country] = None
 
-def back_track_degree_heuristic(adj_dict, colors):
+    def checkMap(self, country):
+        return check_map(self.state, country, self.adj_dict)
 
-    heap = [( -len(value), key ) for key , value in adj_dict.items() ]
-    heapq.heapify(heap)
-    state = { key : None for key in adj_dict.keys() }
-    back_track( heap , state, adj_dict, colors)
-    print (state)
+    def backTrack(self):
+        if self.settingAll():
+            return True
 
-def back_track_MRV(adj_dict, colors):
-    heap, state, colors = initilaize(adj_dict, colors)
-    def back_track_mrv(heap , state, adj_dict, colors):
-        dkeysrate(dcolor)
-        dpop()
-        dsetcolordir()
-            decrease_keys(country)
-            dforward(back_track_mrv)
-            increase_keys(country)
-        state[country] = None
-        heapq.heappush(heap , (- len(colors[country]), country) )
+        country = self.pop()
+        for color in self.opthions(country):
+            self.setColor(country, color)
+            if self.checkMap(country) and self.backTrack():
+                return True
+            self.undoColor(country, color)
         return False
 
+    def getAdjCountrys(self, country):
+        for adj in self.adj_dict[country]:
+            yield adj
+
+    def getAdjNotSeted(self, country):
+        for adj in self.getAdjCountrys(country):
+            if self.state[adj] is None:
+                yield adj
+
+class BackTrackDH(BackTrack):
+    def __init__(self, adj_dict, colors):
+        super().__init__(adj_dict, colors)
+        self.heap = [( -len(adj_countrys) , country) for country , adj_countrys
+          in self.adj_dict.items() ]
+        heapq.heapify(self.heap)
+
+    def pop(self):
+        _, country = heapq.heappop(self.heap)
+        return country
+
+class BackTrackMRV(BackTrackDH):
+    def __init__(self, adj_dict, colors):
+        super().__init__(adj_dict, colors)
+        self.heap = [( len(self.colors[country]) , country) for country
+          in self.adj_dict.keys() ]
+        heapq.heapify(self.heap)
+        self.undo = {}
 
 
-    #//   state.color() <--- color
-    #//   for v in state.edges() :
-    #//       if color in v.options:
-    #//           v.options -= color
-    #//   heapify and then continue back_track
-    #//   if is impassible to fill with that colors
-    #//   then for v in state.edges() :
-    #//       if color not in v.opthions:
-    #//           v.opthions += color
-    #//   heapify and try other color / the heapify can white
-    back_track_mrv( heap , state, adj_dict, colors)
-    print (state)
+    def isOpthion(self, country, color):
+        return color in self.colors[country]
 
-def back_track_FC(adj_dict, colors):
-    heap, state, colors = initilaize(adj_dict, colors)
+    def push(self, country):
+        heapq.heappush(self.heap, (len(self.colors[country]), country))
 
-    def back_track_fc(heap , state, adj_dict, colors):
-        dkeysrate(ddcolor)
-        dpop()
-        dsetcolordir()
-            if decrease_keys(country):
-                dforward(back_track_fc)
-            increase_keys(country)
-        state[country] = None
-        heapq.heappush(heap , (- len(colors[country]), country) )
-        return False
-    back_track_fc( heap , state, adj_dict, colors)
-    print (state)
+    def removeColorOpthion(self, country, color):
+        self.colors[country].remove( color )
+        self.push( country )
 
-def back_track_LCV(adj_dict, colors):
-    heap, state, colors = initilaize(adj_dict, colors)
+    def addColorOpthion(self, country, color):
+        self.colors[country].append( color )
+        self.push( country )
 
-    def back_track_lcv(heap , state, adj_dict, colors):
-        dkeysratelcv(dcoloroptionscount)
+    def setColor(self, country, color):
+        self.state[country] = color
+        for adj in self.getAdjNotSeted(country):
+            if self.isOpthion(adj, color):
+                self.removeColorOpthion( adj , color )
+                self.undo[ (country, adj) ] = color
 
-        def colorvalue(country , color):
-            state[country] = color
-            ret = decrease_keys(country)
-            increase_keys(country)
-            return ret
+    def undoColor(self, country, color):
+        for adj in self.getAdjNotSeted(country):
+            if (country, adj) in self.undo:
+                self.addColorOpthion(adj , self.undo[(country, adj)])
 
-        dpop()
-        for color in sorted(colors[country], key = lambda color : - colorvalue(country , color)):
-            state[country] = color
-            decrease_keys(country)
-            dforward(back_track_lcv)
-            increase_keys(country)
-        state[country] = None
-        heapq.heappush(heap , (- len(colors[country]), country) )
-        return False
-    back_track_lcv( heap , state, adj_dict, colors)
-    print (state)
+        self.state[country] = None
 
-def fast_back_track(adj_dict, colors):
-    pass
+class BackTrackFC(BackTrackMRV):
+    def __init__(self, adj_dict, colors):
+        super().__init__(adj_dict, colors)
+        self.map_check_flag = True
 
+    def checkMap(self, country):
+        return self.map_check_flag and super().checkMap(country)
+
+    def removeColorOpthion(self, country, color):
+        self.map_check_flag = len(self.colors[country]) > 1
+        super().removeColorOpthion(country, color)
+    def undoColor(self, country, color):
+        self.map_check_flag = True
+        super().undoColor(country, color)
+
+class BackTrackLCV():
+    def __init__(self, adj_dict, colors, backTrackType):
+        self.back_track_obj = backTrackType(adj_dict, colors)
+        self.state = self.back_track_obj.state
+        self.back_track_obj.opthions = self.opthions
+        self.tempcolors =  { color : 0 for color in colors }
+    def backTrack(self):
+        self.back_track_obj.backTrack()
+
+    def opthions(self, country):
+        tempcolors = deepcopy(self.tempcolors)
+        obj = self.back_track_obj
+        for adj in obj.getAdjNotSeted(country):
+            for color in obj.colors[adj]:
+                tempcolors[color] += 1
+        return sorted( [ color for color ,count in tempcolors.items()],
+         key = lambda color : tempcolors[color] )
+
+def back_track_degree_heuristic( adj_dict, colors ):
+    back_track_obj = BackTrackDH( adj_dict, colors )
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
+def back_track_MRV( adj_dict, colors ):
+    back_track_obj = BackTrackMRV( adj_dict, colors )
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
+def back_track_FC( adj_dict, colors ):
+    back_track_obj = BackTrackFC( adj_dict, colors )
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
+def back_track_LCV( adj_dict, colors ):
+    back_track_obj = BackTrackLCV( adj_dict, colors, BackTrackFC)
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
 
 import os
 import threading
@@ -163,6 +156,6 @@ if __name__ == '__main__' :
     back_track_MRV( graph , COLORS )
     back_track_FC( graph , COLORS )
     def callable():
-        back_track_LCV( graph , COLORS)
+         back_track_LCV( graph , COLORS)
     thread = threading.Thread(target=callable)
     thread.start()
--- exercises/ex/ex11/tempex11_improve_backtrack.py
+++ exercises/ex/ex11/tempex11_improve_backtrack.py
@@ -1,176 +1,150 @@
 from ex11_map_coloring import read_adj_file, COLORS, check_map
-
-
-
-
-
-
-
-
-
-
-
-
+from copy import deepcopy
 import heapq
 
-from copy import deepcopy , copy
+class BackTrack:
+    def __init__ (self, adj_dict, colors):
+        self.heap = adj_dict.keys()
+        self.colors = { **{ 0  : deepcopy(colors)   } ,
+        **{key : deepcopy(colors) for key in adj_dict.keys() }}
+        self.state = { key : None for key in adj_dict.keys() }
+        self.adj_dict = adj_dict
+    #abs
+    def pop(self):
+        return self.heap.pop()
 
-def initilaize(adj_dict, colors):
-    heap = [( 0 , key ) for key , value in adj_dict.items() ]
-    heapq.heapify(heap)
-    state = { key : None for key in adj_dict.keys() }
-    colors = { **{ 0  : deepcopy(colors)   } ,
-    **{key : deepcopy(colors) for key in adj_dict.keys() }}
-    return heap, state, colors
+    def settingAll(self):
+        return len(self.heap) == 0
 
-def back_track(heap , state, adj_dict, colors, hooking=False):
-    if len(heap) == 0 :
-        return True
-    negdegree , country = heapq.heappop(heap)
-    for color in colors :
-        state[country] = color
-        if check_map(state, country, adj_dict) and \
-         back_track(heap, state, adj_dict, colors):
+    def opthions(self , country):
+        return self.colors[country]
+
+    def setColor(self, country, color):
+        self.state[country] = color
+    def undoColor(self, country, color):
+        self.state[country] = None
+
+    def checkMap(self, country):
+        return check_map(self.state, country, self.adj_dict)
+
+    def backTrack(self):
+        if self.settingAll():
             return True
-    state[country] = None
-    heapq.heappush(heap , (negdegree , country) )
-    return False
 
-
-def back_track_degree_heuristic(adj_dict, colors):
-
-    heap = [( -len(value), key ) for key , value in adj_dict.items() ]
-    heapq.heapify(heap)
-    state = { key : None for key in adj_dict.keys() }
-    back_track( heap , state, adj_dict, colors)
-    print (state)
-
-def back_track_MRV(adj_dict, colors):
-    heap, state, colors = initilaize(adj_dict, colors)
-    def back_track_mrv(heap , state, adj_dict, colors):
-        def decrease_keys(country):
-            ret =  True
-            for adj_country in adj_dict[country]:
-                if state[adj_country] is None:
-                    if state[country] in colors[adj_country]:
-                        colors[adj_country].remove( state[country]  )
-                        heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-            return ret
-        def increase_keys(country):
-            for adj_country in adj_dict[country]:
-                if state[adj_country] is None:
-                    if state[country] not in colors[adj_country]:
-                        colors[adj_country].append( state[country]  )
-                        heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-        if len(heap) == 0 :
-            return True
-        negdegree , country = heapq.heappop(heap)
-        for color in colors[country] :
-            state[country] = color
-            decrease_keys(country)
-            if check_map(state, country, adj_dict) and \
-             back_track_mrv(heap, state, adj_dict, colors):
+        country = self.pop()
+        for color in self.opthions(country):
+            self.setColor(country, color)
+            if self.checkMap(country) and self.backTrack():
                 return True
-            increase_keys(country)
-        state[country] = None
-        heapq.heappush(heap , (- len(colors[country]), country) )
+            self.undoColor(country, color)
         return False
 
+    def getAdjCountrys(self, country):
+        for adj in self.adj_dict[country]:
+            yield adj
+
+    def getAdjNotSeted(self, country):
+        for adj in self.getAdjCountrys(country):
+            if self.state[adj] is None:
+                yield adj
+
+class BackTrackDH(BackTrack):
+    def __init__(self, adj_dict, colors):
+        super().__init__(adj_dict, colors)
+        self.heap = [( -len(adj_countrys) , country) for country , adj_countrys
+          in self.adj_dict.items() ]
+        heapq.heapify(self.heap)
+
+    def pop(self):
+        _, country = heapq.heappop(self.heap)
+        return country
+
+class BackTrackMRV(BackTrackDH):
+    def __init__(self, adj_dict, colors):
+        super().__init__(adj_dict, colors)
+        self.heap = [( len(self.colors[country]) , country) for country
+          in self.adj_dict.keys() ]
+        heapq.heapify(self.heap)
+        self.undo = {}
 
 
-    #//   state.color() <--- color
-    #//   for v in state.edges() :
-    #//       if color in v.options:
-    #//           v.options -= color
-    #//   heapify and then continue back_track
-    #//   if is impassible to fill with that colors
-    #//   then for v in state.edges() :
-    #//       if color not in v.opthions:
-    #//           v.opthions += color
-    #//   heapify and try other color / the heapify can white
-    back_track_mrv( heap , state, adj_dict, colors)
-    print (state)
+    def isOpthion(self, country, color):
+        return color in self.colors[country]
 
-def back_track_FC(adj_dict, colors):
-    heap, state, colors = initilaize(adj_dict, colors)
+    def push(self, country):
+        heapq.heappush(self.heap, (len(self.colors[country]), country))
 
-    def back_track_fc(heap , state, adj_dict, colors):
-        def decrease_keys(country):
-            ret =  True
-            for adj_country in adj_dict[country]:
-                if state[adj_country] is None:
-                    if state[country] in colors[adj_country]:
-                        colors[adj_country].remove( state[country]  )
-                        if len(colors[adj_country]) == 0 :
-                            return False
-                        heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-            return ret
-        def increase_keys(country):
-            for adj_country in adj_dict[country]:
-                if state[adj_country] is None:
-                    if state[country] not in colors[adj_country]:
-                        colors[adj_country].append( state[country]  )
-                        heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-        if len(heap) == 0 :
-            return True
-        negdegree , country = heapq.heappop(heap)
-        for color in colors[country] :
-            state[country] = color
-            if decrease_keys(country):
-                if check_map(state, country, adj_dict) and \
-                 back_track_fc(heap, state, adj_dict, colors):
-                    return True
-            increase_keys(country)
-        state[country] = None
-        heapq.heappush(heap , (- len(colors[country]), country) )
-        return False
-    back_track_fc( heap , state, adj_dict, colors)
-    print (state)
+    def removeColorOpthion(self, country, color):
+        self.colors[country].remove( color )
+        self.push( country )
 
-def back_track_LCV(adj_dict, colors):
-    heap, state, colors = initilaize(adj_dict, colors)
+    def addColorOpthion(self, country, color):
+        self.colors[country].append( color )
+        self.push( country )
 
-    def back_track_lcv(heap , state, adj_dict, colors):
-        def decrease_keys(country):
-            ret =  0
-            for adj_country in adj_dict[country]:
-                if state[adj_country] is None:
-                    if state[country] in colors[adj_country]:
-                        colors[adj_country].remove( state[country]  )
-                        heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
-                        ret += len(colors[adj_country])
-            return ret
-        def increase_keys(country):
-            for adj_country in adj_dict[country]:
-                if state[adj_country] is None:
-                    if state[country] not in colors[adj_country]:
-                        colors[adj_country].append( state[country]  )
-                        heapq.heappush(heap, ( - len(colors[adj_country])  , adj_country))
+    def setColor(self, country, color):
+        self.state[country] = color
+        for adj in self.getAdjNotSeted(country):
+            if self.isOpthion(adj, color):
+                self.removeColorOpthion( adj , color )
+                self.undo[ (country, adj) ] = color
 
-        def colorvalue(country , color):
-            state[country] = color
-            ret = decrease_keys(country)
-            increase_keys(country)
-            return ret
-        if len(heap) == 0 :
-            return True
-        negdegree , country = heapq.heappop(heap)
-        for color in sorted(colors[country], key = lambda color : - colorvalue(country , color)):
-            state[country] = color
-            decrease_keys(country)
-            if check_map(state, country, adj_dict) and \
-             back_track_lcv(heap, state, adj_dict, colors):
-                return True
-            increase_keys(country)
-        state[country] = None
-        heapq.heappush(heap , (- len(colors[country]), country) )
-        return False
-    back_track_lcv( heap , state, adj_dict, colors)
-    print (state)
+    def undoColor(self, country, color):
+        for adj in self.getAdjNotSeted(country):
+            if (country, adj) in self.undo:
+                self.addColorOpthion(adj , self.undo[(country, adj)])
 
-def fast_back_track(adj_dict, colors):
-    pass
+        self.state[country] = None
 
+class BackTrackFC(BackTrackMRV):
+    def __init__(self, adj_dict, colors):
+        super().__init__(adj_dict, colors)
+        self.map_check_flag = True
+
+    def checkMap(self, country):
+        return self.map_check_flag and super().checkMap(country)
+
+    def removeColorOpthion(self, country, color):
+        self.map_check_flag = len(self.colors[country]) > 1
+        super().removeColorOpthion(country, color)
+    def undoColor(self, country, color):
+        self.map_check_flag = True
+        super().undoColor(country, color)
+
+class BackTrackLCV():
+    def __init__(self, adj_dict, colors, backTrackType):
+        self.back_track_obj = backTrackType(adj_dict, colors)
+        self.state = self.back_track_obj.state
+        self.back_track_obj.opthions = self.opthions
+        self.tempcolors =  { color : 0 for color in colors }
+    def backTrack(self):
+        self.back_track_obj.backTrack()
+
+    def opthions(self, country):
+        tempcolors = deepcopy(self.tempcolors)
+        obj = self.back_track_obj
+        for adj in obj.getAdjNotSeted(country):
+            for color in obj.colors[adj]:
+                tempcolors[color] += 1
+        return sorted( [ color for color ,count in tempcolors.items()],
+         key = lambda color : tempcolors[color] )
+
+def back_track_degree_heuristic( adj_dict, colors ):
+    back_track_obj = BackTrackDH( adj_dict, colors )
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
+def back_track_MRV( adj_dict, colors ):
+    back_track_obj = BackTrackMRV( adj_dict, colors )
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
+def back_track_FC( adj_dict, colors ):
+    back_track_obj = BackTrackFC( adj_dict, colors )
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
+def back_track_LCV( adj_dict, colors ):
+    back_track_obj = BackTrackLCV( adj_dict, colors, BackTrackFC)
+    back_track_obj.backTrack()
+    print(back_track_obj.state)
 
 import os
 import threading
@@ -182,6 +156,6 @@ if __name__ == '__main__' :
     back_track_MRV( graph , COLORS )
     back_track_FC( graph , COLORS )
     def callable():
-        back_track_LCV( graph , COLORS)
+         back_track_LCV( graph , COLORS)
     thread = threading.Thread(target=callable)
     thread.start()
